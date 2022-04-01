7aed8e24f4397bfc01574b974d1bd47fb6315aea
Matthew McClain <matthew.mcclain@gmail.com>
2022-04-01 08:30:58 -0700

    longer timer for sweetalert hack delay fix

--- src/components/TaskPlayer/index.vue
+++ src/components/TaskPlayer/index.vue
@@ -81,7 +81,7 @@ export default {
         // TODO: Think about how to get rid of this delay haaaack
         // which is needed because of sweetaltert using
         // a body style !important to get its transition to work
-        await new Promise( res => setTimeout(res, 150))
+        await new Promise( res => setTimeout(res, 260))
         this.$emit('taskCorrect')
       }
       else if (this.error) await taskIncorrectSwal(this.error)
