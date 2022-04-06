cb5ef4ece66b1dec582820f27c20ab36b8a85183
Matthew McClain <matthew.mcclain@gmail.com>
2022-04-05 20:21:58 -0700

    update default map ref

--- src/store/mapIds.js
+++ src/store/mapIds.js
@@ -1,5 +1,5 @@
 const defaultMapIds = [
     'aade7f79-6c64-4aaa-9d2c-d6eec2fcdcd6',
-    '20518dd5-b8f0-4a86-b06e-61c0c79e1d4d'
+    'b837be15-e029-49d6-9e7c-84947ccf5156'
 ]
 export default defaultMapIds
