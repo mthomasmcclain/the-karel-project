ceec8e3c63f0f33f85dbc84c110afd70ff55b0ad
Matthew McClain <matthew.mcclain@gmail.com>
2022-04-05 20:18:13 -0700

    new expert content map

--- src/store/mapIds.js
+++ src/store/mapIds.js
@@ -1,4 +1,5 @@
 const defaultMapIds = [
-    'aade7f79-6c64-4aaa-9d2c-d6eec2fcdcd6'
+    'aade7f79-6c64-4aaa-9d2c-d6eec2fcdcd6',
+    '20518dd5-b8f0-4a86-b06e-61c0c79e1d4d'
 ]
 export default defaultMapIds
