48023204a485f4976d1d54f11b5f3379e90aad5b
Matthew McClain <matthew.mcclain@gmail.com>
2022-04-05 20:37:01 -0700

    expert map for mario

--- src/store/mapIds.js
+++ src/store/mapIds.js
@@ -1,5 +1,6 @@
 const defaultMapIds = [
     'aade7f79-6c64-4aaa-9d2c-d6eec2fcdcd6',
-    'b837be15-e029-49d6-9e7c-84947ccf5156'
+    'b837be15-e029-49d6-9e7c-84947ccf5156',
+    '339941d5-fac6-4111-b06f-bceae65db2a4'
 ]
 export default defaultMapIds
