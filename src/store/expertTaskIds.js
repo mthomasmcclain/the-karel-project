e581c9818d0f379cc11320bc036817686bb6acf5
M Thomas McClain <m.thom.mcclain@gmail.com>
2023-04-19 22:09:13 -0700

    add some translated expert content

--- src/store/expertTaskIds.js
+++ src/store/expertTaskIds.js
@@ -1,7 +1,7 @@
 const defaultTaskIds = [
     '27e882b8-319c-43f4-bf21-189a28cb76d5', // Task 1: Functions
     'e5debff9-5c62-4faa-a3b5-f66a84d84223', // Task 2: Complete Function 
-    '9d3e1587-5f9c-4ef6-acdb-7f6ec394fbf9', // Task 3: Simplify Code
-    '19565ddc-e796-46bf-a092-e0431bded132', // Task 4: Debug Function
+    '19565ddc-e796-46bf-a092-e0431bded132', // Task 4: Simplify Fn
+    '15b79699-247b-40c8-a6aa-c8a6eaf03f9e', // Task 6: While Conclusion
 ]
 export default defaultTaskIds
--- src/store/mapIdToDifficulty.js
+++ src/store/mapIdToDifficulty.js
@@ -1,4 +1,5 @@
 const mapIdToDifficulty = {
-    'dc9cf101-4d9d-4b4b-aa1f-09176948a580' : 'beginner'
+    '750218f5-f1b3-4fb2-88e3-5f03d60a70e2' : 'beginner',
+    'cdf79725-e7c8-41f4-9e76-1fa06cb54f45' : 'advanced'
 }
 export default mapIdToDifficulty
