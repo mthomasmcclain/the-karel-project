242e8940e1e550f63fb1876bc3de772dd8a4267a
Matthew McClain <matthewmcclain@Matthews-MacBook-Pro.local>
2023-11-10 16:44:58 -0800

    update dev server port

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 9899 },
   plugins: [
     vue(),
     basicSsl()
   ]
 })
+
