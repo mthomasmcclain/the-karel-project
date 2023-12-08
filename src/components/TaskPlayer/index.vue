d669c3b9eab73df55d42c20a9469dc8267f97f27
Matthew McClain <matthewmcclain@Dajungs-MBP.localdomain>
2023-12-08 09:38:27 -0800

    flow in player translations

--- src/components/TaskPlayer/index.vue
+++ src/components/TaskPlayer/index.vue
@@ -3,21 +3,21 @@
     <div class="left-col">
       <div class="instructions-and-reset-wrapper">
         <div class="instructions-box">
-          <b>Challenge:</b> {{ localT(task.instructions) }}
+          <b>{{ t('challenge') }}:</b> {{ localT(task.instructions) }}
           <p v-if="task.maxBlocks" class="max-blocks-p">
             Solve the challenge using <b>{{task.maxBlocks}}</b> or fewer blocks. The current code uses <b :style="`color: ${blocksUsed > task.maxBlocks ? 'red' : 'green'};`">{{ blocksUsed }}</b> blocks.
           </p>
         </div>
-        <button v-if="!karelBlockly?.settings.disabled" class="karel-button reset" @click="resetTask">Reset Code</button>
+        <button v-if="!karelBlockly?.settings.disabled" class="karel-button reset" @click="resetTask">{{ t('reset-code') }}</button>
       </div>
 
       <div class="worlds-wrapper">
         <div class="world-col left">
-          <h2>{{ t('start-world') }}:</h2>
+          <h2>{{ t('start') }}:</h2>
           <KarelWorldRenderer :world="world" key="active-world" />
         </div>
         <div class="world-col right">
-          <h2>Goal:</h2>
+          <h2>{{ t('goal') }}:</h2>
           <KarelWorldRenderer :world="activePostWorld" key="goal-world"/>
         </div>
       </div>
@@ -75,7 +75,7 @@
       />
     </div>
   </div>
-  <div v-else>loading...</div>
+  <div v-else> ... {{ t('loading') }} ... </div>
 </template>
 
 <script>
@@ -220,10 +220,10 @@ export default {
     },
 
     getScenarioLabel(i) {
-      const start = `Scenario ${i+1}: `
-      let end = 'Not Tried'
-      if (this.correctScenarios[i]) end = 'Solved'
-      else if (this.correctScenarios[i] === false) end = 'Not Solved'
+      const start = `${this.t('scenario')} ${i+1}: `
+      let end = this.t('not-tried')
+      if (this.correctScenarios[i]) end = this.t('solved')
+      else if (this.correctScenarios[i] === false) end = this.t('not-solved')
       return start + end
     },
     startPlaying() {
