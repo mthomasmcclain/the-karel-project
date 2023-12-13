285ad9c613680335b7d4588f96b02ac893e60b5f
Matthew McClain <matthewmcclain@matthews-mbp.lan>
2023-12-12 18:38:33 -0800

    attempt push update to expert tasks

--- src/store/taskIds.js
+++ src/store/taskIds.js
@@ -1,7 +1,4 @@
 const defaultTaskIds = [
-    // '3057cf71-42be-4ead-ab20-683f8e085efb',
-    // '3d001131-390b-4165-8f3b-6a931155c34d',
-    // '762deb0b-7746-471f-b654-352db9063b4f',
-    '52db5a19-c52c-490e-b2f8-38e9a8d2693a'
+    '678b9eb0-2627-4c5b-9c72-b7bb9ae743d1'
 ]
 export default defaultTaskIds
