be731b1982e0dc4363e7eaf68881edcf3342a95b
Matthew McClain <matthewmcclain@matthews-mbp.lan>
2023-12-12 19:00:36 -0800

    add turn around expert task

--- src/store/taskIds.js
+++ src/store/taskIds.js
@@ -1,4 +1,5 @@
 const defaultTaskIds = [
-    '678b9eb0-2627-4c5b-9c72-b7bb9ae743d1'
+    '678b9eb0-2627-4c5b-9c72-b7bb9ae743d1', // the big spiral
+    '266086e4-32f2-439e-a405-e9aeb5f545f0', // learn to turn around
 ]
 export default defaultTaskIds
