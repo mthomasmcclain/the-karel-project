9e390d1af412ac003cae86d5c1970bbdb44a13ca
Matthew McClain <mthommcclain@gmail.com>
2023-12-31 08:36:18 -0800

    re-add ad map button to wild player

--- src/views/KarelPlayer.vue
+++ src/views/KarelPlayer.vue
@@ -47,7 +47,7 @@
         </div>
       </div>
 
-      <div v-if="activeTab === 'Custom'" class="add-map-button" @click="addMap" >
+      <div v-if="activeTab === 'custom'" class="add-map-button" @click="addMap" >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green">
           <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/>
         </svg>
